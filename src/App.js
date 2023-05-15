efcf8fb48efc7b0e3c7da966e8d64a8f777b4151
sm-muhaimen <amuhaimenpro829@gmail.com>
2023-05-15 23:20:57 +0600

    input and button added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+
 function App() {
+  let [num, setNum] = useState("");
+  let [add, setAdd] = useState(0);
+  let [sub, setSub] = useState(0);
+  let [multiply, setMultiply] = useState(0);
+  let [divide, setDivide] = useState(0);
+
+  let handleAdd = (e) => {
+    setNum(e.target.value);
+  };
+
+  let handleClick = () => {
+    setAdd(num);
+  };
+
   return (
     <>
-      <h1>React Calculator</h1>
-      <p>hello</p>;
+      <div className="one">
+        <h1>React Calculator</h1>
+        <input onChange={handleAdd} />
+        <h3>Addition</h3>
+        <input />
+        <h3>Substitution</h3>
+        <input />
+        <h3>Multiply</h3>
+        <input />
+        <h3>Divide</h3>
+        <button onClick={handleClick}>Click</button>
+
+        <h2>{add}</h2>
+      </div>
     </>
   );
 }
