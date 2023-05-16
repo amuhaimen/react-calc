551a3610fdbe5979d4d0c652d8fd8f40a9683ca4
sajid <sajid.mern2201@gmail.com>
2023-05-16 23:10:07 +0600

    added addition

--- src/App.js
+++ src/App.js
@@ -7,29 +7,69 @@ function App() {
   let [multiply, setMultiply] = useState(0);
   let [divide, setDivide] = useState(0);
 
+
   let handleAdd = (e) => {
     setNum(e.target.value);
+
+  };
+  let handleSub = (e) => {
+    setSub(e.target.value);
+    
+  };
+  let handleMul = (e) => {
+    setMultiply(e.target.value);
+  };
+  let handleDiv = (e) => {
+    setDivide(e.target.value);
   };
 
+  let addition = (a,b) => {
+    return a + b
+  }
+  let substitution = (a,b) => {
+    return a - b
+  }
+  let multiplyFunc = (a,b) => {
+    return a * b
+  }
+  let Divide = (a,b) => {
+    return a / b
+  }
+
   let handleClick = () => {
-    setAdd(num);
+
+    if(! num == ""){
+      setAdd(num);
+    }
+
+    if(num == ""){
+      setSub(num);
+      console.log(num)
+    }
+    
   };
+
+ 
+  
 
   return (
     <>
       <div className="one">
         <h1>React Calculator</h1>
-        <input onChange={handleAdd} />
+
         <h3>Addition</h3>
-        <input />
+        <input onChange={handleAdd} />
         <h3>Substitution</h3>
-        <input />
+        <input onChange={handleSub}/>
         <h3>Multiply</h3>
-        <input />
+        <input onChange={handleMul}/>
         <h3>Divide</h3>
+        <input onChange={handleDiv}/><br/>
+      
         <button onClick={handleClick}>Click</button>
 
-        <h2>{add}</h2>
+        <h2>{0}</h2>
+        <h2>{0}</h2>
       </div>
     </>
   );
