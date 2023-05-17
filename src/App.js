46e79f963a68db995a41d83890a91a5f386ade3c
tushari789 <tushar789imran@gmail.com>
2023-05-18 03:26:30 +0600

    sum, Minus and Maltiply is added

--- src/App.js
+++ src/App.js
@@ -1,78 +1,91 @@
-import { useState } from "react";
+import React from 'react'
+import Practice from "./components/Practice"
 
-function App() {
-  let [num, setNum] = useState("");
-  let [add, setAdd] = useState(0);
-  let [sub, setSub] = useState(0);
-  let [multiply, setMultiply] = useState(0);
-  let [divide, setDivide] = useState(0);
+const App = () => {
+  return (
+    <Practice/>
+  )
+}
+
+export default App
 
 
-  let handleAdd = (e) => {
-    setNum(e.target.value);
 
-  };
-  let handleSub = (e) => {
-    setSub(e.target.value);
+// import { useState } from "react";
+
+// function App() {
+//   let [num, setNum] = useState("");
+//   let [add, setAdd] = useState(0);
+//   let [sub, setSub] = useState(0);
+//   let [multiply, setMultiply] = useState(0);
+//   let [divide, setDivide] = useState(0);
+
+
+//   let handleAdd = (e) => {
+//     setNum(e.target.value);
+
+//   };
+//   let handleSub = (e) => {
+//     setSub(e.target.value);
     
-  };
-  let handleMul = (e) => {
-    setMultiply(e.target.value);
-  };
-  let handleDiv = (e) => {
-    setDivide(e.target.value);
-  };
+//   };
+//   let handleMul = (e) => {
+//     setMultiply(e.target.value);
+//   };
+//   let handleDiv = (e) => {
+//     setDivide(e.target.value);
+//   };
 
-  let addition = (a,b) => {
-    return a + b
-  }
-  let substitution = (a,b) => {
-    return a - b
-  }
-  let multiplyFunc = (a,b) => {
-    return a * b
-  }
-  let Divide = (a,b) => {
-    return a / b
-  }
+//   let addition = (a,b) => {
+//     return a + b
+//   }
+//   let substitution = (a,b) => {
+//     return a - b
+//   }
+//   let multiplyFunc = (a,b) => {
+//     return a * b
+//   }
+//   let Divide = (a,b) => {
+//     return a / b
+//   }
 
-  let handleClick = () => {
+//   let handleClick = () => {
 
-    if(! num == ""){
-      setAdd(num);
-    }
+//     if(! num == ""){
+//       setAdd(num);
+//     }
 
-    if(num == ""){
-      setSub(num);
-      console.log(num)
-    }
+//     if(num == ""){
+//       setSub(num);
+//       console.log(num)
+//     }
     
-  };
+//   };
 
  
   
 
-  return (
-    <>
-      <div className="one">
-        <h1>React Calculator</h1>
+//   return (
+//     <>
+//       <div className="one">
+//         <h1>React Calculator</h1>
 
-        <h3>Addition</h3>
-        <input onChange={handleAdd} />
-        <h3>Substitution</h3>
-        <input onChange={handleSub}/>
-        <h3>Multiply</h3>
-        <input onChange={handleMul}/>
-        <h3>Divide</h3>
-        <input onChange={handleDiv}/><br/>
+//         <h3>Addition</h3>
+//         <input onChange={handleAdd} />
+//         <h3>Substitution</h3>
+//         <input onChange={handleSub}/>
+//         <h3>Multiply</h3>
+//         <input onChange={handleMul}/>
+//         <h3>Divide</h3>
+//         <input onChange={handleDiv}/><br/>
       
-        <button onClick={handleClick}>Click</button>
+//         <button onClick={handleClick}>Click</button>
 
-        <h2>{0}</h2>
-        <h2>{0}</h2>
-      </div>
-    </>
-  );
-}
+//         <h2>{0}</h2>
+//         <h2>{0}</h2>
+//       </div>
+//     </>
+//   );
+// }
 
-export default App;
+// export default App;
