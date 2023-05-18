00206f6ab476a02d2fa05432bc361591b0919d3a
urme-akter <112346871+urme-akter@users.noreply.github.com>
2023-05-19 00:13:48 +0600

    Design done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
-import { useState } from "react";
-
 function App() {
-  let [num, setNum] = useState("");
-  let [add, setAdd] = useState(0);
-  let [sub, setSub] = useState(0);
-  let [multiply, setMultiply] = useState(0);
-  let [divide, setDivide] = useState(0);
-
-  let handleAdd = (e) => {
-    setNum(e.target.value);
-  };
-
-  let handleClick = () => {
-    setAdd(num);
-  };
-
   return (
     <>
-      <div className="one">
-        <h1>React Calculator</h1>
-        <input onChange={handleAdd} />
-        <h3>Addition</h3>
-        <input />
-        <h3>Substitution</h3>
-        <input />
-        <h3>Multiply</h3>
-        <input />
-        <h3>Divide</h3>
-        <button onClick={handleClick}>Click</button>
-
-        <h2>{add}</h2>
-      </div>
+      <section className="flex">
+        <div className="left bg-[#36ADBD] w-[60%]  px-[39px]">
+          <div className="up flex">
+            <div className=" w-[30%] pt-[80px]">
+              <h3 className="text-center font-inter font-bold text-4xl text-white">
+                ADD
+              </h3>
+              <input className="w-full py-[13px] pl-[20px] mt-[21px]" />
+            </div>
+            <div className=" w-[37%] pt-[143px]">
+              <a href="#">
+                <div className="mx-auto font-inter font-bold text-2xl py-[10px] px-[77px] w-[235px]  text-black bg-white">
+                  Button
+                </div>
+              </a>
+            </div>
+            <div className=" w-[30%] text-right pt-[80px]">
+              <h3 className="text-center font-inter font-bold text-4xl text-white">
+                Division
+              </h3>
+              <input className="w-full py-[13px] pl-[20px] mt-[21px]" />
+            </div>
+          </div>
+          <div className="down flex">
+            <div className=" w-[30%] pt-[500px] pb-[72px]">
+              <h3 className="text-center font-inter font-bold text-4xl text-white">
+                Minus
+              </h3>
+              <input className="w-full py-[13px] pl-[20px] mt-[21px]" />
+            </div>
+            <div className=" w-[39%] "></div>
+            <div className=" w-[30%]  pt-[500px] pb-[72px]">
+              <h3 className="text-center font-inter font-bold text-4xl text-white">
+                Multiplication
+              </h3>
+              <input className="w-full py-[13px] pl-[20px] text-right mt-[21px]" />
+            </div>
+          </div>
+        </div>
+        <div className="right bg-[#36ADBD] w-[40%] h-[878px] text-center">
+          <h2 className="text-white font-inter font-bold text-[96px] pt-[63px]">
+            List
+          </h2>
+        </div>
+      </section>
     </>
   );
 }
