452c7430f8ea560297bbae02f874710f8784827d
sm-muhaimen <amuhaimenpro829@gmail.com>
2023-05-19 01:59:55 +0600

    design and calculator marge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
- 
-import React from 'react'
-import Practice from "./components/Practice"
-
+import React, { useState, useEffect, useRef } from "react";
 const App = () => {
-  return (
-    <Practice/>
-  )
- 
-function App() {
- 
+  const [commonState, setCommonState] = useState("");
+
+  // useRef state
+  const [sumState, setSumState] = useState("");
+  const previousSumRef = useRef(0);
+
+  //Minus state
+  const [minusStare, setMinusState] = useState("");
+  const previousMinusRef = useRef(0);
+
+  //Maltiply state
+  const [maltiplyState, setMaltiplyState] = useState("");
+
+  //Maltiply state
+  const [divisionState, setDivisionState] = useState("");
+
+  //sum
+  const handleAdd = (e) => {
+    setSumState(e.target.value);
+  };
+
+  //minus
+
+  let handleMinus = (e) => {
+    setMinusState(e.target.value);
+  };
+  //maltiply
+
+  let handleMaltiply = (e) => {
+    setMaltiplyState(e.target.value);
+  };
+
+  //division
+  let handleDivision = (e) => {
+    setDivisionState(e.target.value);
+  };
+
+  // let handleClear = () => {
+  //   setSumState("");
+  //   setMinusState("");
+  // };
+
+  let handleAddClick = () => {
+    if (minusStare === "" && maltiplyState === "" && divisionState === "") {
+      setCommonState(parseInt(previousSumRef.current) + parseInt(sumState));
+      previousSumRef.current = sumState;
+      // setSumState("");
+    } else if (
+      sumState === "" &&
+      maltiplyState === "" &&
+      divisionState === ""
+    ) {
+      setCommonState(parseInt(commonState) - parseInt(minusStare));
+      previousMinusRef.current = minusStare;
+      // setMinusState("");
+    } else if (sumState === "" && minusStare === "" && divisionState === "") {
+      setCommonState(parseInt(commonState) * parseInt(maltiplyState));
+      // setMaltiplyState("");
+    } else if (sumState === "" && minusStare === "" && maltiplyState === "") {
+      setCommonState(parseInt(commonState) / parseInt(divisionState));
+      // previousDivisionRef.current = divisionState;
+      // setMaltiplyState("");
+    }
+  };
+
   return (
     <>
       <section className="flex">
@@ -18,20 +74,34 @@ function App() {
               <h3 className="text-center font-inter font-bold text-4xl text-white">
                 ADD
               </h3>
-              <input className="w-full py-[13px] pl-[20px] mt-[21px]" />
+              <input
+                placeholder="Sum"
+                type="text"
+                onChange={handleAdd}
+                className="w-full py-[13px] pl-[20px] mt-[21px]"
+              />
             </div>
             <div className=" w-[37%] pt-[143px]">
               <a href="#">
-                <div className="mx-auto font-inter font-bold text-2xl py-[10px] px-[77px] w-[235px]  text-black bg-white">
+                <div
+                  onClick={handleAddClick}
+                  className="mx-auto font-inter font-bold text-2xl py-[10px] px-[77px] w-[235px]  text-black bg-white"
+                >
                   Button
                 </div>
               </a>
             </div>
+            <h2>0</h2>
             <div className=" w-[30%] text-right pt-[80px]">
               <h3 className="text-center font-inter font-bold text-4xl text-white">
                 Division
               </h3>
-              <input className="w-full py-[13px] pl-[20px] mt-[21px]" />
+              <input
+                placeholder="Division"
+                type="text"
+                onChange={handleDivision}
+                className="w-full py-[13px] pl-[20px] mt-[21px]"
+              />
             </div>
           </div>
           <div className="down flex">
@@ -39,29 +109,39 @@ function App() {
               <h3 className="text-center font-inter font-bold text-4xl text-white">
                 Minus
               </h3>
-              <input className="w-full py-[13px] pl-[20px] mt-[21px]" />
+              <input
+                placeholder="Minus"
+                type="text"
+                onChange={handleMinus}
+                className="w-full py-[13px] pl-[20px] mt-[21px]"
+              />
             </div>
             <div className=" w-[39%] "></div>
             <div className=" w-[30%]  pt-[500px] pb-[72px]">
               <h3 className="text-center font-inter font-bold text-4xl text-white">
                 Multiplication
               </h3>
-              <input className="w-full py-[13px] pl-[20px] text-right mt-[21px]" />
+              <input
+                placeholder="Maltiply"
+                type="text"
+                onChange={handleMaltiply}
+                className="w-full py-[13px] pl-[20px] text-right mt-[21px]"
+              />
             </div>
           </div>
         </div>
         <div className="right bg-[#36ADBD] w-[40%] h-[878px] text-center">
           <h2 className="text-white font-inter font-bold text-[96px] pt-[63px]">
-            List
+            History list
           </h2>
+          <ul>
+            <li>{commonState}</li>
+          </ul>
         </div>
       </section>
- 
-  
- 
+      <h1>Main value: {commonState} </h1>
     </>
   );
- 
-}
+};
 
-export default App
+export default App;
--- src/conponents/Practice.js
+++ src/conponents/Practice.js
@@ -1,79 +1,79 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function App() {
-  const [commonState, setCommonState] = useState("");
+  // const [commonState, setCommonState] = useState("");
 
-  // useRef state
-  const [sumState, setSumState] = useState("");
-  const previousSumRef = useRef(0);
+  // // useRef state
+  // const [sumState, setSumState] = useState("");
+  // const previousSumRef = useRef(0);
 
-  //Minus state
-  const [minusStare, setMinusState] = useState("");
-  const previousMinusRef = useRef(0);
+  // //Minus state
+  // const [minusStare, setMinusState] = useState("");
+  // const previousMinusRef = useRef(0);
 
-  //Maltiply state
-  const [maltiplyState, setMaltiplyState] = useState("");
+  // //Maltiply state
+  // const [maltiplyState, setMaltiplyState] = useState("");
 
-  //Maltiply state
-  const [divisionState, setDivisionState] = useState("");
+  // //Maltiply state
+  // const [divisionState, setDivisionState] = useState("");
 
-  //sum
-  const handleAdd = (e) => {
-    setSumState(e.target.value);
-  };
-
-  //minus
-
-  let handleMinus = (e) => {
-    setMinusState(e.target.value);
-  };
-  //maltiply
-
-  let handleMaltiply = (e) => {
-    setMaltiplyState(e.target.value);
-  };
-
-  //division
-  let handleDivision = (e) => {
-    setDivisionState(e.target.value);
-  };
-
-  // let handleClear = () => {
-  //   setSumState("");
-  //   setMinusState("");
+  // //sum
+  // const handleAdd = (e) => {
+  //   setSumState(e.target.value);
   // };
 
-  let handleAddClick = () => {
-    if (minusStare === "" && maltiplyState === "" && divisionState === "") {
-      setCommonState(parseInt(previousSumRef.current) + parseInt(sumState));
-      previousSumRef.current = sumState;
-      // setSumState("");
-    } else if (
-      sumState === "" &&
-      maltiplyState === "" &&
-      divisionState === ""
-    ) {
-      setCommonState(parseInt(commonState) - parseInt(minusStare));
-      previousMinusRef.current = minusStare;
-      // setMinusState("");
-    } else if (sumState === "" && minusStare === "" && divisionState === "") {
-      setCommonState(parseInt(commonState) * parseInt(maltiplyState));
-      // setMaltiplyState("");
-    } else if (sumState === "" && minusStare === "" && maltiplyState === "") {
-      setCommonState(parseInt(commonState) / parseInt(divisionState));
-      // previousDivisionRef.current = divisionState;
-      // setMaltiplyState("");
-    }
-  };
+  // //minus
+
+  // let handleMinus = (e) => {
+  //   setMinusState(e.target.value);
+  // };
+  // //maltiply
+
+  // let handleMaltiply = (e) => {
+  //   setMaltiplyState(e.target.value);
+  // };
+
+  // //division
+  // let handleDivision = (e) => {
+  //   setDivisionState(e.target.value);
+  // };
+
+  // // let handleClear = () => {
+  // //   setSumState("");
+  // //   setMinusState("");
+  // // };
+
+  // let handleAddClick = () => {
+  //   if (minusStare === "" && maltiplyState === "" && divisionState === "") {
+  //     setCommonState(parseInt(previousSumRef.current) + parseInt(sumState));
+  //     previousSumRef.current = sumState;
+  //     // setSumState("");
+  //   } else if (
+  //     sumState === "" &&
+  //     maltiplyState === "" &&
+  //     divisionState === ""
+  //   ) {
+  //     setCommonState(parseInt(commonState) - parseInt(minusStare));
+  //     previousMinusRef.current = minusStare;
+  //     // setMinusState("");
+  //   } else if (sumState === "" && minusStare === "" && divisionState === "") {
+  //     setCommonState(parseInt(commonState) * parseInt(maltiplyState));
+  //     // setMaltiplyState("");
+  //   } else if (sumState === "" && minusStare === "" && maltiplyState === "") {
+  //     setCommonState(parseInt(commonState) / parseInt(divisionState));
+  //     // previousDivisionRef.current = divisionState;
+  //     // setMaltiplyState("");
+  //   }
+  // };
 
   return (
     <div>
-      <input placeholder="Sum" type="text" onChange={handleAdd} />
-      <input placeholder="Minus" type="text" onChange={handleMinus} />
-      <input placeholder="Maltiply" type="text" onChange={handleMaltiply} />
-      <input placeholder="Division" type="text" onChange={handleDivision} />
+      {/* <input placeholder="Sum" type="text" onChange={handleAdd} /> */}
+      {/* <input placeholder="Minus" type="text" onChange={handleMinus} /> */}
+      {/* <input placeholder="Maltiply" type="text" onChange={handleMaltiply} /> */}
+      {/* <input placeholder="Division" type="text" onChange={handleDivision} /> */}
 
-      <button onClick={handleAddClick}>Submit</button>
+      {/* <button onClick={handleAddClick}>Submit</button> */}
       {/* <button onClick={handleClear}>Clear</button> */}
       <h2>Sum: {commonState}</h2>
       <h2>Minus: {commonState}</h2>
